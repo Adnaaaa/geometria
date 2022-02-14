0cad13ce0acca2dfdd4025a7ea7f0bcc7777891e
butro <butro@W10_SI.CEFIRE2022.LOCAL>
2022-02-14 16:59:48 +0100

    Documentar es de sabios aunque leerla sea pesado

---
+++ geometria/ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_ABG"},{"p":"<Unnamed>","l":"FiguraGeometrica_ABG"},{"p":"<Unnamed>","l":"Rectangulo_ABG"},{"p":"<Unnamed>","l":"Triangulo_ABG"}];updateSearchResults();
